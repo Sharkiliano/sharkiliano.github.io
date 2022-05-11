a44b00a7733f183493bf27e77c6139580a3ebe3c
Sharkiliano <105375947+Sharkiliano@users.noreply.github.com>
2022-05-11 12:26:55 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,54 @@
+/*
+quando clicar no pokemon na listagem temos que esconder o atual e mostrar o correspondente ao selecionado
+
+pra isso vamos trabalhar com 2 elementos
+1- listagem
+2- cartão pokemon
+
+precisa criar duas variaveis no js paraa trabalhar com os elementos da tela
+
+vamos precisar trabalhar com evento de clique feito pelo usuario na listagem de pokemon
+
+-remover a classe averto só do cartão que está aberto
+-ao clicar em um pokemon da listagem pegamos o id do pokemon para saber qual cartão mostrar 
+-remover classe ativa que marca o pokemon selecionado
+
+adicionar a classe ativo no selecionado
+*/
+
+// criar duas variaveis  para trabalhar com os elementos na tela
+
+const listaSelecaoPokemons = document.querySelectorAll('.instancia')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+const submenu = document.querySelectorAll('.submenu')
+
+
+
+listaSelecaoPokemons.forEach(instancia => {
+    // vamos precisar trabalhar com evento de clique feito pelo usuario na listagem de pokemon
+
+    instancia.addEventListener('click', () => {
+        // -remover a classe averto só do cartão que está aberto
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+        
+
+        // ao clicar em um pokemon da listagem pegamos o id do pokemon para saber qual cartão mostrar 
+       const idPokemonSelecionado = instancia.attributes.id.value
+
+       const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+       const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+       cartaoPokemonParaAbrir.classList.add('aberto')
+
+    //  -remover classe ativa que marca o pokemon selecionado
+        const pokemonAtivoNalistagem = document.querySelector('.ativo')
+        pokemonAtivoNalistagem.classList.remove('ativo')
+        
+        
+        
+    // adicionar a classe ativo no selecionado
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+    })
+    
+})
